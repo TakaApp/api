18c3c6d5b2983e05f6848f283251385a3f07208b
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-13 17:09:47 +0200

    (routes) add a route on /

---
+++ routes/docs.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// GetDocs returns the docs on / and a 200 Code
+// we cant remove it:
+// it is important otherwise services (eq cloudfare) will health check on /
+// and will think the server died
+func GetDocs(c echo.Context) error {
+	return c.String(http.StatusOK, "42")
+}
