bfef36832d5998ada8502899dddfcc5e347715ee
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-13 13:43:08 +0200

    (routes) POST/Trip GetTrip : basic version

---
+++ routes/trip.go
@@ -0,0 +1,65 @@
+package routes
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type getTripRequest struct {
+	From     string `json:"from"`
+	To       string `json:"to"`
+	ArriveAt string `json:"arriveAt"`
+	LeaveAt  string `json:"leaveAt`
+	Date     string `json:"date"`
+}
+
+// GetTrip returns possible itineraries between two points
+func GetTrip(c echo.Context) error {
+	// build a GetTripRequest
+	request := new(getTripRequest)
+	err := c.Bind(request)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return c.String(http.StatusBadRequest, "Sorry (1)")
+	}
+
+	// create a request to our OTP server
+	req, err := http.NewRequest("GET", "http://gtfs.aksels.io/otp/routers/default/plan", nil)
+	if err != nil {
+		log.Print(err)
+		return c.String(http.StatusInternalServerError, "Sorry (2)")
+	}
+
+	// add OTP required params
+	q := req.URL.Query()
+	q.Add("fromPlace", request.From)
+	q.Add("toPlace", request.To)
+	q.Add("time", request.LeaveAt)
+	q.Add("date", request.Date)
+	q.Add("mode", "TRANSIT,WALK")
+	q.Add("maxWalkDistance", "500000")
+	q.Add("arriveBy", "false")
+
+	req.URL.RawQuery = q.Encode()
+
+	// request our server
+	response, err := http.Get(req.URL.String())
+	if err != nil {
+		log.Print(err)
+		return c.String(http.StatusInternalServerError, "Sorry (3)")
+	}
+
+	rawResponseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Print(err)
+		return c.String(http.StatusInternalServerError, "Sorry (4)")
+	}
+
+	response.Body.Close()
+
+	return c.String(http.StatusOK, string(rawResponseBody))
+}
