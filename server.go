8c6a1303adf92fa2af123d72dc94c131305578ee
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 10:56:42 +0200

    (server.go) add cors * for dev. purpose

--- server.go
+++ server.go
@@ -3,10 +3,16 @@ package main
 import (
 	"github.com/TakaApp/api/routes"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
+	}))
 
 	e.GET("/", routes.GetDocs)
 	e.POST("/trip", routes.GetTrip)
