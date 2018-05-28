381f6ec0c80de47993d3cc622c8f402f7473ab7b
Aksels Ledins <aksels.ledins@gmail.com>
2018-05-28 17:14:03 +0200

    (routes) GetSearchStop : let algolia handle because we wont write another search engine tool

--- routes/search-stop.go
+++ routes/search-stop.go
@@ -1,16 +1,56 @@
 package routes
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
-	gtfs "github.com/TakaApp/api/gtfs/stops"
+	"github.com/algolia/algoliasearch-client-go/algoliasearch"
 	"github.com/labstack/echo"
 )
 
+type Stop struct {
+	Name      string  `json:"stop_name"`
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lon"`
+}
+
+// GetSearchStop returns a list of stops
 func GetSearchStop(c echo.Context) error {
 	text := c.Param("text")
 
-	results := gtfs.SearchStop(text)
+	client := algoliasearch.NewClient(os.Getenv("ALGOLIA_APP_ID"), os.Getenv("ALGOLIA_SECRET"))
+	index := client.InitIndex("stops")
 
-	return c.JSON(http.StatusOK, results)
+	settings := algoliasearch.Map{
+		"page":                 0,
+		"hitsPerPage":          5,
+		"attributesToRetrieve": []string{"stop_name", "stop_lat", "stop_lon"},
+	}
+
+	res, err := index.Search(text, settings)
+
+	if err != nil {
+		log.Printf("err: %v\n", err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	var hits []Stop
+	for _, result := range res.Hits {
+		fmt.Printf("result: %v\n", result)
+
+		stopName := result["stop_name"].(string)
+
+		stopLatitude := result["stop_lat"].(float64)
+		stopLongitude := result["stop_lon"].(float64)
+
+		hits = append(hits, Stop{
+			Name:      stopName,
+			Latitude:  stopLatitude,
+			Longitude: stopLongitude,
+		})
+	}
+
+	return c.JSON(http.StatusOK, hits)
 }
