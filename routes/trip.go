389c4010d83bf1f71a40f09101be85ee180872e7
Aksels Ledins <aksels.ledins@gmail.com>
2019-02-05 18:27:44 +0100

    (routes) GetTrip fetch stop details in legs
    notes:
    - it need a good refactor
    - we can simplify a lot of things here
    - the code is not pretty
    - and it definitly need some documentation
    
    major problems:
    - it's slower (like a lot)
    
    actions:
    - try to reduce computation, api calls
    options:
    - create a new route, it should be the best option. Fetch on runtime the stops we need. Maybe when the user opens a itineray on its app ?

--- routes/trip.go
+++ routes/trip.go
@@ -66,6 +66,80 @@ func GetTrip(c echo.Context) error {
 		log.Print(err)
 	}
 
+	for iIDX, itinerary := range result.Plan.Itineraries {
+		fmt.Println("----------------")
+		for lIDX, leg := range itinerary.Legs {
+			fromStopID := leg.From.StopID
+			toStopID := leg.To.StopID
+
+			routeID := leg.RouteID
+			if fromStopID == "" || toStopID == "" || leg.RouteID == "" {
+				continue
+			}
+
+			// fetch patterns
+			patternReq, _ := http.NewRequest("GET", fmt.Sprintf("http://gtfs.aksels.io/otp/routers/default/index/routes/%s/patterns", routeID), nil)
+			patternReqResult, _ := http.Get(patternReq.URL.String())
+
+			var patterns []Pattern
+			patternReqResponseBody, _ := ioutil.ReadAll(patternReqResult.Body)
+			if err := json.Unmarshal(patternReqResponseBody, &patterns); err != nil {
+				log.Print(err)
+			}
+
+			fmt.Printf("(!) %s >> %s\n", fromStopID, toStopID)
+			// fmt.Printf("patterns %v\n", patterns)
+
+			var stopDetails []Stop
+			for _, pattern := range patterns {
+				stopDetails = nil
+
+				// fetch data about a single pattern
+				singlePatternReq, _ := http.NewRequest("GET", fmt.Sprintf("http://gtfs.aksels.io/otp/routers/default/index/patterns/%s", pattern.ID), nil)
+				singlePatternReqResult, _ := http.Get(singlePatternReq.URL.String())
+
+				var p Pattern
+				singlePatternReqResponseBody, _ := ioutil.ReadAll(singlePatternReqResult.Body)
+				if err := json.Unmarshal(singlePatternReqResponseBody, &p); err != nil {
+					log.Print(err)
+				}
+
+				haveFoundFrom := false
+				haveFoundTo := false
+				for _, stop := range p.Stops {
+					if stop.ID == fromStopID {
+						haveFoundFrom = true
+					}
+					if stop.ID == toStopID {
+						haveFoundTo = true
+					}
+
+					if haveFoundFrom || haveFoundTo {
+						stopDetails = append(stopDetails, stop)
+					}
+
+					if haveFoundFrom && haveFoundTo {
+						break
+					}
+				}
+
+				if haveFoundFrom && haveFoundTo {
+					// maybe need to reverse?
+					if stopDetails[0].ID == leg.To.StopID {
+						// reverse method someone ?
+						for i := len(stopDetails)/2 - 1; i >= 0; i-- {
+							opp := len(stopDetails) - 1 - i
+							stopDetails[i], stopDetails[opp] = stopDetails[opp], stopDetails[i]
+						}
+					}
+					break
+				}
+			}
+
+			result.Plan.Itineraries[iIDX].Legs[lIDX].Stops = stopDetails
+		}
+	}
+
 	response.Body.Close()
 
 	simplifiedResult, _ := json.Marshal(result)
